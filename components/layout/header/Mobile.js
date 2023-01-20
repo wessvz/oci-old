cf70c909efe3458c04b95303339446f30a0d6147
wessvz <wesselvanzuuk@gmail.com>
2023-01-20 13:10:54 +0100

    update

--- components/layout/header/Mobile.js
+++ components/layout/header/Mobile.js
@@ -1,13 +1,6 @@
 import styles from "./Mobile.module.css";
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import MenuItem from "./MenuItem";
-import {
-  Ranking,
-  Coding,
-  Roadmap,
-  Message,
-  People,
-} from "../../../public/icons/Outline";
 
 export default function Mobile() {
   return (
@@ -27,32 +20,28 @@ export default function Mobile() {
             </div>
           </NavigationMenu.Trigger>
           <NavigationMenu.Content className={styles.content}>
-            <div className={styles.MobileHeader}>OCI</div>
+            <div className={styles.MobileHeader}></div>
             <nav>
               <ul className={styles.list}>
                 <MenuItem
                   link="/products"
                   title="Products"
                   description="Bereik meer en kom verder"
-                  icon={<Ranking />}
                 />
                 <MenuItem
                   link="/products"
                   title="Sustainability"
                   description="Applicaties voor het moderne web"
-                  icon={<Coding />}
                 />
                 <MenuItem
                   link="/about"
                   title="About us"
                   description="Begin met het waarom"
-                  icon={<Roadmap />}
                 />
                 <MenuItem
                   link="/contact"
                   title="Contact"
                   description="Begin met het waarom"
-                  icon={<Roadmap />}
                 />
               </ul>
             </nav>

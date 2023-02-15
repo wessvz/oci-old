5b0d50841ab7192a883a1723a86cba734ec56f30
wessvz <wesselvanzuuk@gmail.com>
2023-02-15 17:49:34 +0100

    update

--- components/layout/header/Mobile.js
+++ components/layout/header/Mobile.js
@@ -1,7 +1,11 @@
 import styles from "./Mobile.module.css";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import MenuItem from "./MenuItem";
-import { useState } from "react";
+import LargeButton from "@/components/button/LargeButton";
+import logo from "@/public/images/logo-oci-white.svg";
 
 export default function Mobile() {
   const [isOpen, setIsOpen] = useState(false);
@@ -28,7 +32,11 @@ export default function Mobile() {
             </div>
           </NavigationMenu.Trigger>
           <NavigationMenu.Content className={styles.content}>
-            <div className={styles.MobileHeader}></div>
+            <div className={styles.header}>
+              <Link href="/" onSelect={toggleMenu}>
+                <Image alt="OCI" src={logo} className={styles.svg} />
+              </Link>
+            </div>
             <nav>
               <ul className={styles.list}>
                 <MenuItem
@@ -42,17 +50,23 @@ export default function Mobile() {
                   onSelect={toggleMenu}
                 />
                 <MenuItem
-                  link="/products"
-                  title="Sustainability"
-                  onSelect={toggleMenu}
-                />
-                <MenuItem
                   link="/contact"
                   title="Contact"
                   onSelect={toggleMenu}
                 />
+                <MenuItem
+                  link="/products"
+                  title="Sustainability"
+                  onSelect={toggleMenu}
+                />
               </ul>
             </nav>
+            <div className={styles.footer}>
+              <LargeButton
+                link="/contact"
+                buttonText="contact opnemen met sales"
+              />
+            </div>
           </NavigationMenu.Content>
         </NavigationMenu.Item>
       </NavigationMenu.List>

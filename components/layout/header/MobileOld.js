6d51a17d56a90f6e167c4a56edeccf722e692e78
wessvz <wesselvanzuuk@gmail.com>
2023-02-15 16:44:40 +0100

    update

---
+++ components/layout/header/MobileOld.js
@@ -0,0 +1,41 @@
+import styles from "./MobileOld.module.css";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import MenuItem from "./MenuItem";
+
+export default function Mobile() {
+  return (
+    <NavigationMenu.Root>
+      <NavigationMenu.List>
+        <NavigationMenu.Item>
+          <NavigationMenu.Trigger
+            onPointerMove={(event) => event.preventDefault()}
+            onPointerLeave={(event) => event.preventDefault()}
+            className={styles.NavigationTrigger}
+            aria-label="Menu"
+          >
+            <div className={styles.hamburger}>
+              <span className={styles.line}></span>
+              <span className={styles.line}></span>
+              <span className={styles.line}></span>
+            </div>
+          </NavigationMenu.Trigger>
+          <NavigationMenu.Content className={styles.content}>
+            <div className={styles.MobileHeader}></div>
+            <nav>
+              <ul className={styles.list}>
+                <MenuItem link="/about" title="About us" />
+                <MenuItem link="/products" title="Products" />
+                <MenuItem link="/products" title="Sustainability" />
+                <MenuItem link="/contact" title="Contact" />
+              </ul>
+            </nav>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
+      </NavigationMenu.List>
+
+      <div className={styles.ViewportPosition}>
+        <NavigationMenu.Viewport className={styles.NavigationMenuViewport} />
+      </div>
+    </NavigationMenu.Root>
+  );
+}

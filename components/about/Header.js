698636d5f38fff0fea030a3c09031076e433afd4
wessvz <wesselvanzuuk@gmail.com>
2023-03-03 15:37:55 +0100

    update

--- components/about/Header.js
+++ components/about/Header.js
@@ -14,10 +14,11 @@ export default function Header() {
             Moving markets with meaningful innovations.
           </h2>
           <p className={styles.copy}>
-            Pick up an incoming call from your iPhone, kick back with your
-            favorite shows on Apple TV, listen to music. You can even create a
-            stereo pair.paired in the same room create left and right channels
-            for fully immersive audio
+            At OCI, we understand that sustainability, innovation, and quality
+            are critical components of our customers' success. That's why we are
+            committed to providing the energy and water industry with
+            outstanding products and solutions. We are proud of our reputation
+            as a customer-centric and trustworthy supplier.
           </p>
         </div>
       </div>

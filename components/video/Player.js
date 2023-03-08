300694771255d08d162c5da94849251f96f4a093
wessvz <wesselvanzuuk@gmail.com>
2023-03-08 14:04:26 +0100

    update

--- components/video/Player.js
+++ components/video/Player.js
@@ -4,11 +4,20 @@ function Player() {
   return (
     <ReactPlayer
       url="./video/fossilfree-desktop.mp4"
-      loop="true"
-      playing="true"
+      loop={true}
+      playing={true}
       width="100%"
       height="auto"
       muted={true}
+      config={{
+        file: {
+          attributes: {
+            playing: true,
+            loop: true,
+            muted: true,
+          },
+        },
+      }}
     />
   );
 }

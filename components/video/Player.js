56fdc78e12c6a7cac95942634c5fd6b465b932a7
wessvz <wesselvanzuuk@gmail.com>
2023-03-13 17:35:03 +0100

    update

--- components/video/Player.js
+++ components/video/Player.js
@@ -1,20 +1,23 @@
 import dynamic from "next/dynamic";
 import styles from "./Player.module.css";
+import useMediaQuery from "@/lib/hooks/useMediaQuery";
 
 const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 export default function Player() {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
   return (
     <div className={styles.videoWrapper}>
       <ReactPlayer
-        url="./video/fossilfree-desktop.mp4"
+        url={isMobile ? "./video/mobile.mp4" : "./video/desktop.mp4"}
         loop={true}
         playing={true}
         muted={true}
         playsinline="true"
         width="100%"
         height="100%"
-        className={styles.test}
+        className={styles.player}
       />
     </div>
   );

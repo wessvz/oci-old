426667c6e921a6b5a29b7a8faaf31c74ff969d41
wessvz <wesselvanzuuk@gmail.com>
2023-03-20 15:57:26 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import styles from "../styles/Home.module.css";
 import Head from "next/head";
 import Main from "../components/hero/Main";
-import magazijn from "@/public/images/magazijn.webp";
-import products from "@/public/images/product.webp";
-import Panel from "@/components/layout/content/Panel";
-import Proposition from "@/components/section/Proposition";
-import Title from "@/components/section/Title";
+import Section from "@/components/layout/Section";
+import Copy from "@/components/layout/Copy";
+import Title from "@/components/layout/Title";
+import Eyebrow from "@/components/layout/Eyebrow";
+import Wrapper from "@/components/layout/Wrapper";
+import Flex from "@/components/layout/Flex";
+import Panel from "@/components/pages/home/Panel";
+import Proposition from "@/components/pages/home/Proposition";
+import watch2 from "@/public/images/home/watch-2.jpg";
+import tag from "@/public/images/home/airtag.jpg";
+import product from "@/public/images/home/product.jpg";
+import metaalslang from "@/public/images/home/metaalslang.jpg";
 
 export default function Home() {
   return (
@@ -43,59 +50,78 @@ export default function Home() {
 
       <Main />
 
-      <Title
-        kicker="Specialty"
-        title="Customized assemblies"
-        copy="Smart solutions for connecting energy and water grids to domestic homes, office buildings and industrial sites."
-      />
-      <section className={styles.container}>
+      <Section style={1}>
+        <Wrapper style={2} space="extra">
+          <Eyebrow style={1}>What we do</Eyebrow>
+          <Title style={1} width="home">
+            Customized assemblies
+          </Title>
+          <Copy style={2} width="normal">
+            Smart solutions for connecting energy and water grids to domestic
+            homes, office buildings and industrial sites.
+          </Copy>
+        </Wrapper>
+      </Section>
+      <Flex style={1}>
         <Panel
-          kicker="Products"
+          eyebrow="Products"
           title="How we make a difference"
           copy="OCI delivers innovative and sustainable solutions for connecting energy and water networks to homes, offices and industrial sites."
-          image={magazijn}
-          alt="Products made by OCI"
-          buttonText="Learn more"
+          cta="Learn more"
           link="/products"
+          label="Go to products"
+          image={product}
+          alt="voorbeeld"
         />
         <Panel
-          order="reverse"
-          kicker="Company"
+          eyebrow="Company"
           title="Discover the world of OCI"
           copy="Efficiency and sustainability at the forefront. Innovating to transform markets, that's OCI's mission."
-          image={products}
-          alt="Products made by OCI"
-          buttonText="Read more"
-          link="/sustainability"
-        />
-      </section>
-      <Proposition />
-      <Title
-        kicker="What drives us"
-        title="Strong relationships"
-        copy="Creating sustainable impact by delivering unique value to our customers, partners and employees."
-      />
-      <section className={styles.container}>
-        <Panel
+          cta="Learn more"
+          link="/about"
+          label="Go to about OCI"
+          image={tag}
+          alt="voorbeeld"
           order="reverse"
-          kicker="People"
+        />
+      </Flex>
+      <Proposition />
+
+      <Section style={1}>
+        <Wrapper style={2} space="extra">
+          <Eyebrow style={1}>What drives us</Eyebrow>
+          <Title style={1} width="home">
+            Strong relationships
+          </Title>
+          <Copy style={2} width="normal">
+            Creating sustainable impact by delivering unique value to our
+            customers, partners and employees.
+          </Copy>
+        </Wrapper>
+      </Section>
+      <Flex style={1}>
+        <Panel
+          eyebrow="People"
           title="The heart of social entrepreneurship"
           copy="We strive for a better society and believe that every person is equal. No one is more or less than the other. We may therefore call ourselves a PSO 30+ company."
-          image={magazijn}
-          alt="Products made by OCI"
-          buttonText="Learn more"
+          cta="Learn more"
           link="/about"
+          label="Go to products"
+          image={watch2}
+          alt="voorbeeld"
+          order="reverse"
         />
         <Panel
-          kicker="Sustainability"
+          eyebrow="Sustainability"
           title="Together we create change"
-          copy="The transition to sustainability will inevitably impact all of our clients and partners. We are dedicated to providing support and guidance to navigate this transformative period/"
-          image={products}
-          alt="Products made by OCI"
-          buttonText="Read more"
-          link="/sustainability"
+          copy="The transition to sustainability will inevitably impact all of our clients and partners. We are dedicated to providing support and guidance to navigate this transformative period."
+          cta="Learn more"
+          link="/about"
+          label="Go to about OCI"
+          image={metaalslang}
+          alt="voorbeeld"
         />
-      </section>
+      </Flex>
     </>
   );
 }

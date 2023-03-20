12b905b31708d2cf549ad690bb3d8d61a86ccaa3
wessvz <wesselvanzuuk@gmail.com>
2023-03-20 10:35:35 +0100

    update

--- pages/contact.js
+++ pages/contact.js
@@ -26,7 +26,31 @@ export default function ContactPage() {
       <Head>
         <title>OCI BV | Smart solutions for energy and water networks</title>
         <meta name="description" content="Welkom bij OCI BV" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="logo/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="logo/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="logo/favicon-16x16.png"
+        />
+        <link rel="manifest" href="logo/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="logo/safari-pinned-tab.svg"
+          color="#db0a40"
+        />
+        <meta name="msapplication-TileColor" content="#ff0000" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <Hero />

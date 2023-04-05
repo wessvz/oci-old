ce1d39d52033d44d8f79a2ded1ed698f5ea49205
wessvz <wesselvanzuuk@gmail.com>
2023-04-05 19:17:19 +0200

    update

--- components/pages/about/Copy.js
+++ components/pages/about/Copy.js
@@ -3,35 +3,74 @@ import styles from "./Copy.module.css";
 const mission = (
   <div>
     <p className={styles.intro}>
-      Research and development (R&D) teams are the fastest growing at Apple. R&D
-      is also an area where diversity continues to lag across the tech industry.
-      We have several ongoing and upcoming initiatives to help break down
-      systemic barriers and bias in R&D and beyond.
+      A comfortable home is essential for a happy life. Since our founding in
+      2008, we've been dedicated to creating products and solutions that make
+      our homes more comfortable, healthier, and safer.
     </p>
-    <h3 className={styles.headline}>Representation in leadership matters.</h3>
+    <h3 className={styles.headline}>Entrepreneurial</h3>
     <p className={styles.copy}>
-      In addition to Apple programs like Entrepreneur Camp, Impact Accelerator,
-      and the Apple Developer Academy, Apple partners with external
-      organizations to support and serve people from underrepresented
-      communities. Many of our partnerships also provide opportunities for our
-      engineers and other employees to engage with the next generation of
-      innovators by offering their time as mentors and volunteers.
+      As an entrepreneurial company, we are always looking for ways to break new
+      ground and improve. We are proactive in our approach to innovation and
+      take the initiative to get better every day. We make every effort to
+      succeed, learn from our mistakes, and do better.
     </p>
+
+    <h3 className={styles.headline}>Collaborative</h3>
+    <p className={styles.copy}>
+      At OCI, we believe that we are stronger together. We value collaboration
+      and teamwork, both within our company and with our external partners. We
+      share our knowledge and ask each other questions, giving honest feedback
+      to make as much progress as possible. Our development strength is now
+      known to many contacts, and we are proud of the reputation we have built.
+    </p>
+
     <figure className={styles.quote}>
       <blockquote>
         <span className={styles.quoteText}>
-          The DNAs gave me so much, I want to be able to give back, and it is
-          the best way I can give back, I feel, for Apple.
+          Engineering is not just about calculations and designs, it's about
+          making a positive impact on the world around us.
         </span>
       </blockquote>
     </figure>
 
-    <h3 className={styles.headline}>Support for development at every level.</h3>
-    <p>
-      Opportunity starts with equitable access to career development programs
-      and support. From new‑hire orientation to talent planning, we’re embedding
-      programs, processes, and resources that support employees and help current
-      leaders identify and develop future leaders equitably.
+    <h3 className={styles.headline}>Responsible</h3>
+    <p className={styles.copy}>
+      We set ambitious goals and take responsibility for achieving them. We take
+      on development projects from start to finish, and we are committed to
+      delivering high-quality products that meet the needs of our customers. We
+      believe that being responsible means being accountable for our actions and
+      striving for excellence in everything we do.
+    </p>
+
+    <h3 className={styles.headline}>Flexible</h3>
+    <p className={styles.copy}>
+      In the ever-changing energy landscape, we know that flexibility is key. We
+      embrace change and are always open to new ideas and approaches. We are
+      committed to finding smart solutions for the complex challenges of the
+      energy industry, whether it is related to hydrogen, drinking water, or
+      innovations in the metering room. We are prepared to meet the demands of
+      the future energy system, and we are excited to help our customers
+      innovate and grow.
+    </p>
+
+    <h3 className={styles.headline}>Let's make impact together</h3>
+    <p className={styles.copy}>
+      Our commitment to environment, innovation, and quality drives everything
+      we do. We are proud to have contributed to a sustainable energy and water
+      system, and we will continue to push the boundaries of what is possible.
+    </p>
+
+    <p className={styles.copy}>
+      Our customers include Dutch grid operators for heat and gas and water
+      utilities, as well as installation companies and industrial players. We
+      have earned their trust through our reliability, expertise, and commitment
+      to excellence.
+    </p>
+
+    <p className={styles.copy}>
+      So, whether you are looking for innovative solutions for the new energy
+      landscape, or simply seeking to streamline your work, OCI is here to help.
+      Contact us today to learn more.
     </p>
   </div>
 );

1a4334da0a918d4b15a72e265e690ef454d93793
wessvz <wesselvanzuuk@gmail.com>
2023-04-26 09:58:37 +0200

    update

--- components/pages/about/Brand.js
+++ components/pages/about/Brand.js
@@ -1,14 +1,7 @@
 import styles from "./Brand.module.css";
 import Image from "next/image";
-import products from "@/public/images/contact-placeholder.png";
-import apple from "@/public/images/about/apple-placeholder.jpg";
-import connection from "@/public/images/about/connection.jpg";
-import product from "@/public/images/about/product-1.jpg";
-import productBig from "@/public/images/about/product-big.jpg";
+import products from "@/public/images/about/oci-products.jpg";
 import productsBig from "@/public/images/about/products-big-2.jpg";
-import product1 from "@/public/images/about/product-3.jpg";
-import product2 from "@/public/images/about/products-2.jpg";
-import innovation from "@/public/images/about/innovation.jpg";
 import Modal from "./ModalBrand";
 import { mission, goal } from "./Copy";
 
@@ -31,7 +24,7 @@ export default function Brand() {
             <div className={styles.imageWrapper}>
               <Image
                 alt="Pipes"
-                src={productBig}
+                src={products}
                 className={styles.image}
                 width={800}
                 height={600}

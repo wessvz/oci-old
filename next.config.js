93f56facb48bac357774a88f5ed7c7b01428d128
wessvz <wesselvanzuuk@gmail.com>
2023-05-03 12:27:48 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      { source: "/index.html", destination: "/", permanent: true },
+      { source: "/producten.html", destination: "/products", permanent: true },
+      { source: "/technieken.html", destination: "/products", permanent: true },
+      { source: "/contact.html", destination: "/contact", permanent: true },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

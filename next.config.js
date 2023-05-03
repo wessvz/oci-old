6236ded647625948d0a66d454556b77384d69c44
wessvz <wesselvanzuuk@gmail.com>
2023-05-03 12:33:49 +0200

    update

--- next.config.js
+++ next.config.js
@@ -6,6 +6,21 @@ const nextConfig = {
       { source: "/producten.html", destination: "/products", permanent: true },
       { source: "/technieken.html", destination: "/products", permanent: true },
       { source: "/contact.html", destination: "/contact", permanent: true },
+      {
+        source: "/phone/producten.html",
+        destination: "/products",
+        permanent: true,
+      },
+      {
+        source: "/phone/technieken.html",
+        destination: "/products",
+        permanent: true,
+      },
+      {
+        source: "/phone/contact.html",
+        destination: "/contact",
+        permanent: true,
+      },
     ];
   },
 };

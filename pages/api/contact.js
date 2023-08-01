2a1dc08ed192222004c6874b04b18e8fd10816c3
wessvz <wesselvanzuuk@gmail.com>
2023-08-01 11:16:43 +0200

    update

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -10,14 +10,15 @@ export default async (req, res) => {
     Achternaam: ${body.lastname}\r\n
     Email: ${body.email}\r\n
     Telefoon: ${body.phone}\r\n
-    Website: ${body.website}\r\n
+    Bedrijf: ${body.company}\r\n
+    Functie: ${body.role}\r\n
     Bericht: ${body.message}
   `;
 
   await mail.send({
-    to: "wesselvanzuuk@fullstacks.nl",
+    to: "info@ocibv.nl",
     from: "contact@ocibv.nl",
-    subject: "Nieuw bericht via het contactformulier",
+    subject: "Nieuw bericht via het online contactformulier",
     text: message,
     html: message.replace(/\r\n/g, "<br>"),
   });
